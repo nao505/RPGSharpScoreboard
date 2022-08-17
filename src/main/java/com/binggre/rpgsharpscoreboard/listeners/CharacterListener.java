dad7adcbb13f9ca5987d1d0f0d19494b1bf2e06c
nao505 <nao505@naver.com>
2022-08-17 18:47:14 +0900

    ver 1.0.1
    <해결>
    캐릭터 생성 시 스코어보드가 안 나오는 현상

--- src/main/java/com/binggre/rpgsharpscoreboard/listeners/CharacterListener.java
+++ src/main/java/com/binggre/rpgsharpscoreboard/listeners/CharacterListener.java
@@ -3,6 +3,7 @@ package com.binggre.rpgsharpscoreboard.listeners;
 import com.binggre.rpgsharpscoreboard.RPGSharpScoreboard;
 import com.binggre.rpgsharpscoreboard.objects.RPGScoreboard;
 import com.binggre.rpgsharpscoreboard.scheduler.ScoreboardScheduler;
+import com.hj.rpgsharp.rpg.apis.rpgsharp.events.character.CharacterCreateEvent;
 import com.hj.rpgsharp.rpg.apis.rpgsharp.events.character.CharacterLoadEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,6 +11,19 @@ import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scoreboard.Scoreboard;
 
 public class CharacterListener implements Listener {
+
+    @EventHandler
+    public void onCharacterCreate(CharacterCreateEvent event) {
+        RPGScoreboard rpgScoreboard = RPGScoreboard.getInstance();
+        String nickname = event.getRPGPlayer().getNickname();
+        Scoreboard scoreboard = rpgScoreboard.createNewScoreboard();
+        scoreboard.registerNewObjective(nickname, rpgScoreboard.CRITERIA_NAME);
+        rpgScoreboard.getScoreboard().put(nickname, scoreboard);
+        if (!ScoreboardScheduler.isStart()) {
+            BukkitRunnable bukkitRunnable = new ScoreboardScheduler();
+            bukkitRunnable.runTaskTimerAsynchronously(RPGSharpScoreboard.getInstance(), 0, 10);
+        }
+    }
 
     @EventHandler
     public void onCharacterLoad(CharacterLoadEvent event) {
